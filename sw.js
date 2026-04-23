89f12c4bc54dc0217c46b6379ef8f3e3c3bfb6dc
bhoomibazaar6 <bhoomibazaar6@gmail.com>
2026-04-23 17:55:49 +0530

    Add files via upload

---
+++ sw.js
@@ -0,0 +1,41 @@
+// Delhi NCR Metro - Service Worker
+const CACHE_NAME = 'delhi-metro-v1';
+const urlsToCache = [
+  '/',
+  '/index.html'
+];
+
+// Install
+self.addEventListener('install', event => {
+  event.waitUntil(
+    caches.open(CACHE_NAME).then(cache => {
+      return cache.addAll(urlsToCache);
+    })
+  );
+  self.skipWaiting();
+});
+
+// Activate
+self.addEventListener('activate', event => {
+  event.waitUntil(
+    caches.keys().then(cacheNames => {
+      return Promise.all(
+        cacheNames.filter(name => name !== CACHE_NAME)
+          .map(name => caches.delete(name))
+      );
+    })
+  );
+  self.clients.claim();
+});
+
+// Fetch
+self.addEventListener('fetch', event => {
+  event.respondWith(
+    caches.match(event.request).then(response => {
+      if (response) return response;
+      return fetch(event.request).catch(() => {
+        return caches.match('/index.html');
+      });
+    })
+  );
+});
